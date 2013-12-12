c15f1645c4426f2031709e0492b623f7ff79e020
Guillaume Bouteille <duffman@gb2n.org>
2013-12-12 21:20:29 +0100

    Clean up CLI.

--- cli/main.cpp
+++ cli/main.cpp
@@ -31,10 +31,122 @@
     #include <winbase.h>
 #endif
 
+enum DisplayType {
+    fileType,
+    subtree,
+    info,
+    numberOfChildren,
+    size,
+    binary
+};
+#define DISPLAY_TYPE_DEFAULT DisplayType::subtree
+
+struct CLIOptions {
+    std::string filePath;
+    std::vector<std::string> leafs;
+    DisplayType displayType;
+    int maxDepth;
+    CLIOptions() : filePath(), leafs(), displayType(DISPLAY_TYPE_DEFAULT),
+                   maxDepth(-1) {};
+};
+typedef struct CLIOptions CLIOptions;
+
+void print_help()
+{
+    std::cout << "\
+Usage: hexamonkey-cli FILE [OPTIONS] [[LEAF] ...]\n\
+       hexamonkey-cli [--help]\n\
+The hexamonkey CLI analyses a binary file and extracts information from it. \
+It will go down in the file given a list of leafs (which can either be a field\
+ name or an index).\n\
+\n\
+Example: hexamonkey-cli test.mp4 1 type\n\
+This will look for a field named 'type' in the second item of the mp4 file.\n\
+\n\
+Options:\n\
+  -t, --display-type : What to display. It can be :\n\
+     * fileType : file type detected using extension or magic number (leafs \
+are ignored),\n\
+     * subtree : tree describing the last leaf reached,\n\
+     * info : info on the last leaf reached,\n\
+     * numberOfChildren : number of children of the last leaf reached,\n\
+     * size : size of the last leaf reached (in bits) \n\
+     * binary : raw data of the last leaf reached (use with caution) \n\
+  -d, --max-depth : how deep in the subtree should we go,\n\
+                    ignored if type is not subtree\n\
+                    -1 (default) will go as deep as it gets" << std::endl;
+}
+
+
+bool parseArgs(const int argc, const char* const argv[], CLIOptions& options)
+{
+    std::list<std::string> optStr;
+    for(int i = 1; i < argc; ++i)
+        optStr.push_back(argv[i]);
+
+    if (optStr.empty())
+    {
+        std::cerr << "No input specified" << std::endl;
+        return false;
+    }
+    if(optStr.front() == "--help")
+        return false;
+    options.filePath = optStr.front();
+    optStr.pop_front();
+
+    bool moreOptions = true;
+    while(!optStr.empty() && moreOptions)
+    {
+        std::string flag(optStr.front());
+        if(flag == "--display-type" || flag == "-t")
+        {
+            optStr.pop_front();
+            if(optStr.empty())
+                return false;
+
+            std::string typeStr(optStr.front());
+            optStr.pop_front();
+            if(typeStr == "fileType")
+                options.displayType = DisplayType::fileType;
+            else if(typeStr == "subtree")
+                options.displayType = DisplayType::subtree;
+            else if(typeStr == "info")
+                options.displayType = DisplayType::info;
+            else if(typeStr == "numberOfChildren")
+                options.displayType = DisplayType::numberOfChildren;
+            else if(typeStr == "size")
+                options.displayType = DisplayType::size;
+            else if(typeStr == "binary")
+                options.displayType = DisplayType::binary;
+            else
+                return false;
+        } else if(flag == "--max-depth" || flag == "-d")
+        {
+            optStr.pop_front();
+            if(optStr.empty())
+                return false;
+
+            std::stringstream mdStream(optStr.front());
+            mdStream >> options.maxDepth;
+            optStr.pop_front();
+        } else
+        { moreOptions = false; }
+    }
+
+    for(auto& leaf : optStr)
+        options.leafs.push_back(leaf);
+
+    return true;
+}
+
 int main(int argc, char *argv[])
 {
-
-
+    CLIOptions options;
+    if(!parseArgs(argc, argv, options))
+    {
+        print_help();
+        return 1;
+    }
 
 #if defined(PLATFORM_WIN32)
     char buffer[MAX_PATH];
@@ -76,11 +188,6 @@ int main(int argc, char *argv[])
 
 
     File file;
-    if (argc < 2)
-    {
-        std::cerr << "No input specified" << std::endl;
-        return 1;
-    }
     file.setPath(argv[1]);
     if (!file.good())
     {
@@ -93,23 +200,51 @@ int main(int argc, char *argv[])
         std::vector<Object*> objs;
         objs.push_back(module.handle(defaultTypes::file, file));
         Object*child = nullptr;
-        for (int i = 0; i < argc-2; ++i)
+        for (auto& leaf : options.leafs)
         {
-            objs[i]->explore(1);
-            char* child_id = argv[i+2];
-            child = objs[i]->lookUp(child_id);
+            objs[0]->explore(1);
+            const char* child_id = leaf.c_str();
+            child = objs[0]->lookUp(child_id);
             if (child == nullptr) {
-                child = objs[i]->access(std::stoi(child_id));
+                child = objs[0]->access(std::stoi(child_id));
             }
             if(child) {
-                objs.push_back(child);
+                objs.insert(objs.begin(), child);
             } else {
-                std::cerr << "object not found" << std::endl;
+                std::cerr << "Object not found" << std::endl;
                 return 1;
             }
         }
-        objs[objs.size()-1]->explore(-1);
-        std::cout << *(objs[objs.size()-1]) << std::endl;
+        objs[0]->explore(options.maxDepth);
+        switch(options.displayType)
+        {
+            case fileType:
+                std::cout << objs[objs.size()-1]->type() << std::endl;
+                break;
+
+            case info:
+                std::cout << objs[0]->info() << std::endl;
+                break;
+
+            case subtree:
+                std::cout << *(objs[0]) << std::endl;
+                break;
+
+            case numberOfChildren:
+                std::cout << objs[0]->numberOfChildren() << std::endl;
+                break;
+
+            case size:
+                std::cout << objs[0]->size() << std::endl;
+                break;
+
+            case binary:
+                objs[0]->dump(std::cout);
+                break;
+
+            default:
+                break;
+        }
     }
     return 0;
 }
