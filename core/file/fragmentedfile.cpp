ac33891698a6bbccbeb3b2163bd80298d29d2d65
Guillaume Bouteille <duffman@gb2n.org>
2014-03-20 14:46:02 +0100

    Stream dumps.

--- core/file/fragmentedfile.cpp
+++ core/file/fragmentedfile.cpp
@@ -4,7 +4,42 @@
 #include "core/module.h"
 
 FragmentedFile::FragmentedFile(Object *object, Module *module) : File(), _parent(object), _module(module) {
-    _module.handle(object)
+    // should use the module, somehow
+    // test with psi fragments
+
+    auto pid = object->parent()->lookUp("PID", true)->value().toInteger();
+    int n = object->lookUp("psi_syntax_section", true)
+                  ->lookUp("last_section_number", true)
+                  ->value().toInteger();
+    std::cout << "last_section_number: " << n << ", pid: " << pid  << std::endl;
+
+    if(!object->lookUp("psi_begin", true))
+    {
+        std::cout << "psi_begin null!" << std::endl;
+    }
+    _fragments.push_back(object->lookUp("psi_begin", true));
+
+    auto main_obj = object->parent()->parent();
+    auto it = main_obj->begin()+object->parent()->rank()+1;
+    while(n>0) {
+        for(;it!=main_obj->end();it++) {
+
+            if((*it)->lookUp("PID", true)->value().toInteger() == pid) {
+                if(!(*it)->lookUp("psi_fragment", true)) {
+                    std::cout << "rank: " << (*it)->rank() << std::endl;
+                    std::cout << "whowhowho!" << std::endl;
+                }
+                _fragments.push_back((*it)->lookUp("psi_fragment", true));
+                n--;
+                continue;
+            }
+        }
+        if(it == main_obj->end()) {
+            main_obj->exploreSome(128);
+            if(it == main_obj->end())
+                break;
+        }
+    }
 }
 
 
