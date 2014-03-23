85437704b064dd0ddbfe52440795dcb4b3d2fed7
Guillaume Bouteille <duffman@gb2n.org>
2014-03-23 15:03:11 +0100

    Start parsing PES tables.

--- core/file/esfragmentedfile.cpp
+++ core/file/esfragmentedfile.cpp
@@ -6,7 +6,25 @@ uint32_t EsFragmentedFile::maxFragmentNumber = 1000;
 EsFragmentedFile::EsFragmentedFile(Object *object) : FragmentedFile(object), _n(0)
 {
     _pid = object->lookUp("PID", true)->value().toInteger();
-    _fragments.push_back(object->lookUp("payload", true));
+    auto main_obj = _parent->parent();
+    // _fragments.push_back(object->lookUp("payload", true));
+    int currentRank = 0;
+    while(true) {
+        if(currentRank >= main_obj->numberOfChildren()) {
+            main_obj->exploreSome(128);
+            if(currentRank >= main_obj->numberOfChildren())
+                return;
+        }
+        auto it = main_obj->begin()+currentRank;
+        if( (*it)->lookUp("PID", true)->value().toInteger() == _pid
+            && (*it)->lookUp("payload_unit_start_indicator", true)->value().toInteger() == 1)
+        {
+            _fragments.push_back((*it)->lookUp("payload", true));
+            _n++;
+            return;
+        }
+        currentRank++;
+    }
 }
 
 bool EsFragmentedFile::importNextFragment() {
